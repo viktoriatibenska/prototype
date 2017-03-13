43dcaaf1e920e2e16ea9c734a5bf06a49ca77625
Viktória Tibenská <vtibensk8@gmail.com>
2017-03-13 22:54:25 +0100

    delete pattern

--- Web.Api/queries.js
+++ Web.Api/queries.js
@@ -35,7 +35,68 @@ function getAllPatterns(req, res, next) {
 	});
 };
 
-function getSinglePattern(req, res, next) { };
-function createPattern(req, res, next) { };
-function updatePattern(req, res, next) { };
-function removePattern(req, res, next) { };
+function getSinglePattern(req, res, next) {
+  var patternID = parseInt(req.params.id);
+  db.one('select * from pattern where id = $1', patternID)
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ONE pattern'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function createPattern(req, res, next) {
+  db.none('insert into pattern(name, context)' +
+      'values(${name}, ${context})',
+    req.body)
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Inserted one pattern'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+
+function updatePattern(req, res, next) {
+  db.none('update pattern set name=$1, context=$2 where id=$3',
+    [req.body.name, req.body.context, parseInt(req.params.id)])
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Updated pattern'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+};
+
+function removePattern(req, res, next) { 
+  var patternID = parseInt(req.params.id);
+  db.result('delete from pattern where id = $1', patternID)
+    .then(function (result) {
+      /* jshint ignore:start */
+      res.status(200)
+        .json({
+          status: 'success',
+          message: `Removed ${result.rowCount} pattern`
+        });
+      /* jshint ignore:end */
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+
+};
