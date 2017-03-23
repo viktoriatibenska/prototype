64b643f227fcb1dd0eed560915ed546909eb4b4a
Viktória Tibenská <vtibensk8@gmail.com>
2017-03-23 21:53:52 +0100

    create DB script

---
+++ Db.Queries/createDb.sql
@@ -0,0 +1,9 @@
+﻿--DROP DATABASE "BP";
+
+CREATE DATABASE "BP"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Slovak_Slovakia.1250'
+       LC_CTYPE = 'Slovak_Slovakia.1250'
+       CONNECTION LIMIT = -1;
---
+++ Db.Queries/createTables.sql
@@ -0,0 +1,29 @@
+﻿
+
+CREATE TABLE pattern(
+	id serial UNIQUE PRIMARY KEY
+);
+
+CREATE TABLE variation(
+	id serial UNIQUE PRIMARY KEY
+);
+
+CREATE TABLE state(
+	id serial UNIQUE PRIMARY KEY
+);
+
+CREATE TABLE transition(
+	id serial UNIQUE PRIMARY KEY
+);
+
+CREATE TABLE pattern_user(
+	id serial UNIQUE PRIMARY KEY
+);
+
+CREATE TABLE pattern_creator(
+	id serial UNIQUE PRIMARY KEY
+);
+
+CREATE TABLE pattern_student(
+	id serial UNIQUE PRIMARY KEY
+);
