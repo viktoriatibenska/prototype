23bfe5b101a91a0765f9b443761900f60532b1cf
Viktória Tibenská <vtibensk8@gmail.com>
2017-03-25 16:57:12 +0100

    create tables script finalized

--- Db.Queries/createTables.sql
+++ Db.Queries/createTables.sql
@@ -1,19 +1,23 @@
-﻿DROP TABLE IF EXISTS pattern;
-DROP TABLE IF EXISTS variation;
-DROP TABLE IF EXISTS state;
-DROP TABLE IF EXISTS transition;
-DROP TABLE IF EXISTS pattern_user;
-DROP TABLE IF EXISTS pattern_creator;
-DROP TABLE IF EXISTS pattern_student;
+﻿DROP TABLE IF EXISTS pattern CASCADE;
+DROP TABLE IF EXISTS variation CASCADE;
+DROP TABLE IF EXISTS state CASCADE;
+DROP TABLE IF EXISTS transition CASCADE;
+DROP TABLE IF EXISTS system_user CASCADE;
+DROP TABLE IF EXISTS pattern_creator CASCADE;
+DROP TABLE IF EXISTS pattern_progress CASCADE;
+DROP TABLE IF EXISTS contribution CASCADE;
 
 CREATE TABLE variation(
-	id serial UNIQUE PRIMARY KEY NOT NULL
+	id serial UNIQUE PRIMARY KEY NOT NULL,
+	name varchar(200) NOT NULL,
+	note text,
+	created_time timestamp
 );
 
 CREATE TABLE pattern(
 	id serial UNIQUE PRIMARY KEY NOT NULL,
-	name varchar(200) UNIQUE NOT NULL,
 	primary_variation_id int references variation(id),
+	name varchar(200) UNIQUE NOT NULL,
 	context text,
 	forces text,
 	solution text,
@@ -25,7 +29,7 @@ CREATE TABLE pattern(
 
 CREATE TABLE state(
 	id serial UNIQUE PRIMARY KEY NOT NULL,
-	variation_id int references variation(id),
+	variation_id int references variation(id) ON DELETE CASCADE,
 	name varchar(200),
 	description text,
 	position_x real,
@@ -49,9 +53,27 @@ CREATE TABLE system_user(
 );
 
 CREATE TABLE pattern_creator(
-	id serial UNIQUE PRIMARY KEY NOT NULL
+	id serial UNIQUE PRIMARY KEY NOT NULL,
+	user_id int references system_user(id),
+	contribution real
 );
 
-CREATE TABLE pattern_student(
-	id serial UNIQUE PRIMARY KEY NOT NULL
+CREATE TABLE contribution(
+	id serial UNIQUE PRIMARY KEY NOT NULL,
+	variation_id int references variation(id),
+	creator_id int references pattern_creator(id),
+	is_creator boolean NOT NULL
 );
+
+CREATE TABLE pattern_progress(
+	id serial UNIQUE PRIMARY KEY NOT NULL,
+	state_id int references state(id),
+	user_id int references system_user(id),
+	is_completed boolean,
+	score real
+);
+
+ALTER TABLE variation 
+	ADD COLUMN pattern_id int references pattern(id) ON DELETE CASCADE,
+	ADD COLUMN start_state_id int references state(id),
+	ADD COLUMN created_by_id int references pattern_creator(id);
