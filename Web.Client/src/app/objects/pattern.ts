ad998cfd991953334d59906a436d746ec3b4e024
Viktória Tibenská <vtibensk8@gmail.com>
2017-04-01 17:48:35 +0200

    pattern class constructor and few getters/setters

--- Web.Client/src/app/objects/pattern.ts
+++ Web.Client/src/app/objects/pattern.ts
@@ -1,16 +1,56 @@
 import { Variation } from './variation';
 
 export class Pattern {
-    id: number;
-    primaryVariationId: number;
-    name: string;
-    context: string;
-    forces: string;
-    solution: string;
-    discussion: string;
-    patlet: string;
-    rating: number;
-    isPublished: boolean;
+    private id: number;
+    private primaryVariationId: number;
+    private name: string;
+    private context: string;
+    private forces: string;
+    private solution: string;
+    private discussion: string;
+    private patlet: string;
+    private rating: number;
+    private isPublished: boolean;
 
-    variations: Variation[];
+    private variations: Variation[];
+
+    constructor(
+        id: number,
+        primaryVariationId: number,
+        name: string,
+        context: string,
+        forces: string,
+        solution: string,
+        discussion: string,
+        patlet: string,
+        rating: number,
+        isPublished: boolean
+    ){
+        this.id = id;
+        this.primaryVariationId = primaryVariationId;
+        this.name = name;
+        this.context = context;
+        this.forces = forces;
+        this.solution = solution;
+        this.discussion = discussion;
+        this.patlet = patlet;
+        this.rating = rating;
+        this.isPublished = isPublished;
+    }
+
+    setVariations(variations: Variation[]){
+        this.variations = variations;
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    getPrimaryVariationId(){
+        return this.primaryVariationId;
+    }
+
+    getName(){
+        return this.name;
+    }
 }
